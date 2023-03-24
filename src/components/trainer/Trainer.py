708cbe9df8be8aca2b374ef1a0cda65bec66a2c6
prateeksahu147 <prateeksahu147@gmail.com>
2023-03-24 11:00:13 +0530

    trainer is added

---
+++ src/components/trainer/Trainer.py
@@ -0,0 +1,7 @@
+from object_detection.utils import ops as utils_ops
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+### Model preparation variable
+
+class Trainer:
+    pass
---
+++ src/components/trainer/trainer.sh
@@ -0,0 +1,3 @@
+pwd
+
+python ./object_detection_main.py --model_dir=/Users/prateeksahu/PycharmProjects/MLOps-Project/models/training-models/finetune_ssd_resnet101_v1_fpn_640x640_coco17_tpu-8 --pipeline_config_path=/Users/prateeksahu/PycharmProjects/MLOps-Project/models/training-models/finetune_ssd_resnet101_v1_fpn_640x640_coco17_tpu-8/pipeline.config
